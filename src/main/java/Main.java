ffc5b1034e3f4d7a07c2e3636a75d2393e7f877b
Kamil <226016@student.pwr.edu.pl>
2020-01-22 17:56:55 +0100

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,87 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Robot robot1 = null;
+        Robot robot2 = null;
+        Robot robot3 = null;
+        Robot robot4 = null;
+        final Random random = new Random();
+        try {
+            robot1 = new Robot();
+            robot2 = new Robot();
+            robot3 = new Robot();
+            robot4 = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        final Robot finalRobot1 = robot1;
+        final Robot finalRobot2 = robot2;
+        final Robot finalRobot3 = robot3;
+        final Robot finalRobot4 = robot4;
+
+        Thread.sleep(3000);
+
+        new Thread(() -> {
+            while(true) {
+                //finalRobot1.mouseMove(2410, 775);
+                finalRobot1.mouseMove(1770, 630);
+                finalRobot1.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+                finalRobot1.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+                try {
+                    Thread.sleep(180000 + random.nextInt(180000));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+        Thread.sleep(1000);
+
+        new Thread(() -> {
+            //while(true) {
+            while(true){
+                //finalRobot2.mouseMove(1075, 1225);
+                finalRobot2.mouseMove(1085, 870);
+                finalRobot2.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                finalRobot2.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                try {
+                    Thread.sleep(75000 + random.nextInt(75000));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while(true) {
+                finalRobot3.keyPress(17);
+                finalRobot3.delay(100);
+                finalRobot3.keyPress(38);
+                finalRobot3.keyRelease(38);
+                finalRobot3.keyRelease(17);
+                try {
+                    Thread.sleep(20000 + random.nextInt(20000));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                finalRobot3.keyPress(17);
+                finalRobot3.delay(100);
+                finalRobot3.keyPress(40);
+                finalRobot3.keyRelease(40);
+                finalRobot3.keyRelease(17);
+                try {
+                    Thread.sleep(20000 + random.nextInt(20000));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+
+    }
+}
